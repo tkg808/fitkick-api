503ab0678a598955b67b8a5968895589739df0de
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2022-04-13 15:11:17 -0700

    Fix CORS.

--- django_fitkick/settings.py
+++ django_fitkick/settings.py
@@ -130,6 +130,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "https://localhost:8000",
     "https://127.0.0.1:8000",
+    "https://fitkick-api.herokuapp.com",
+
 ]
 
 REST_FRAMEWORK = {
