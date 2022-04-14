fcca5e796b7c57c90230514412efe680abf4b20b
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2022-04-14 12:49:26 -0700

    Nested again.

--- fitkick/serializers.py
+++ fitkick/serializers.py
@@ -16,47 +16,57 @@ class ExerciseSerializer(serializers.ModelSerializer):
 
 
 # Handles how exercises are serialized in workouts.
-# class NestedExerciseSerializer(serializers.PrimaryKeyRelatedField):
+class NestedExerciseSerializer(serializers.PrimaryKeyRelatedField):
 
-#   class Meta:
-#     model = Exercise
-#     # fields = ('name')
+  class Meta:
+    model = Exercise
+    fields = ('name')
 
-#   # Only serializes the exercise's name.
-#   def to_representation(self, obj):
-#     return obj.name
+  # Only serializes the exercise's name.
+  def to_representation(self, obj):
+    return obj.name
 
-#   # # Uses exercise name to find exercise and update list.
-#   def to_internal_value(self, data):
+  # Uses exercise name to find exercise and update list.
+  def to_internal_value(self, data):
     
-#     if not data:
-#       raise serializers.ValidationError(
-#         {'name': 'This field is required'}
-#         )
-#     if not (isinstance (data, str)):
-#       raise serializers.ValidationError("Expecting exercise's name")
+    # data variable should be the name of the exercise (type string).
+    if not data:
+      raise serializers.ValidationError({
+        'name': 'This field is required',
+        })
+    if not (isinstance (data, str)):
+      raise serializers.ValidationError("Expecting exercise's name")
 
-#     obj = Exercise.objects.filter(name=data).values()[0]
+    # Finds the exercise.
+    obj = Exercise.objects.filter(name=data).values()[0]
 
-#     if not obj:
-#       raise serializers.ValidationError("Not exercise with that name.")
+    # Unlikely outcome.
+    if not obj:
+      raise serializers.ValidationError("No exercise with that name.")
 
-#     return obj['id']
+    return obj['id']
 
 
 class WorkoutSerializer(serializers.HyperlinkedModelSerializer):
+  # def update(self, instance, validated_data):
+  #   instance.name = validated_data.get('name', instance.name)
+  #   instance.notes = validated_data.get('notes', instance.notes)
+  #   instance.save()
+  #   return instance
+
   workout_url = serializers.ModelSerializer.serializer_url_field(
     view_name = 'workout_detail',
   )
   
-  # exercises = NestedExerciseSerializer(
-  #   many = True,
-  #   queryset = Exercise.objects.all(),
-  # )
-
-  exercises = ExerciseSerializer(
+  exercises = NestedExerciseSerializer(
     many = True,
+    required = False,
+    queryset = Exercise.objects.all(),
   )
+
+  # exercises = ExerciseSerializer(
+  #   many = True,
+  # )
 
   owner = serializers.ReadOnlyField(
     source = 'owner.username',
